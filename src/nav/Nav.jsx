bf2a5a7eb2decf905bb162242bd02457f60e765e
Zach Ludwick <zachludwick@Zachs-MacBook-Air.local>
2024-12-17 00:10:48 +0000

    more mobile compatability

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -3,25 +3,33 @@ import './nav.css'
 
 const Nav = () => {
 
-  return (
+  const toggleMobileMenu = () => {
+    const navLinks = document.querySelector('.nav-links');
+    const menuBtn = document.querySelector('.mobile-menu-btn');
+    navLinks.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+  };
+  
 
+  return (
     <nav>
         <div className="nav-content">
             <div className="logo">
                 <a href="/"><h2>K-Render UK</h2></a>
             </div>
-            <ul className='nav-links'>
+            <ul className="nav-links">
                 <li><a href="">Home</a></li>
                 <li><a href="">Services</a></li>
                 <li><a href="">Who We Are</a></li>
                 <li><a href="">Get A Quote</a></li>
             </ul>
-            <div class="mobile-menu-btn">
-              <span>Home</span>
-              <span>Services</span>
-              <span>Who Are We</span>
-              <span>Get A Quote</span>
-          </div>
+        </div>
+        <div className="mb-nav">
+          <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </div>
     </nav>
   )
